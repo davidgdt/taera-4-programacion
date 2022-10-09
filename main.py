f0690c2c06ccde726ec2ab0599c503c0c3cf9781
David Garcia <114870623+davidgdt@users.noreply.github.com>
2022-10-09 19:59:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,123 @@
+marcas_guitarra=[fender, Gibson , Prs]   #lista
+modelos_guitarra[telecaster, les paul, silver sky]  #tupla
+
+print(marcas_guitarra)
+print(tupla)
+
+
+print("/n")
+
+print(marcas_guitarra[2:])
+print(modelos_guitarra[1])
+
+print("/n")
+
+print(modelos_guitarra)
+print(marcas_guitarra)
+marcas_guitarra[2]=gretch 
+print(marcas_guitarra)
+print("las tuplas no son modificables")
+
+print("/n")
+
+print(len(marcas_guitarra))
+print(len(modelos_guitarra))
+
+print("/n")
+
+print(fender in marcas_guitarra) 
+print(stratocaster in modelos_guitarra)
+
+print("/n")
+
+marcas_guitarra,append('blues')
+print(marcas guitarra)
+print("no se pueden añadir datos")
+
+print("/n")
+
+marcas_guitarra.pop(2)
+print(marcas_guitarra)
+print("tuplas no modificables")
+
+print("/n")
+
+marcas_coches = {alfa romeo, jaguar, aston martin}
+print(marcas_coches)
+guitarristas = {
+  stratocaster: "SRV" ,
+    telecaster: "keith richards" ,
+      les paul: "slash" ,
+            SG: "angus young" ,
+  
+}
+print(guitarristas)
+print("/n")
+print("no se puede enseñar en los sets")
+print(next(iter(guitarristas)))
+
+print("/n") 
+print("el set no permite realizar cambios")
+guitarristas[4]=guitarristas.pop(lucille)
+guitarristas[4]= 'bb king'
+print(guitarristas)
+
+print("/n")
+
+print('alfa romeo'in marcas_coches)
+print(lucille in guitarristas)
+
+print("/n")
+
+marcas_coches.add('audi')
+guitarristas[flying v]='jimmy H'
+print(marcas_coches)
+print(guitarristas)
+
+print("/n")
+
+guitarristas.pop(sg)
+print(guitarristas)
+
+marcas_coches.discard('aston martin')
+print(marcas_coches)
+
+print("/n")
+
+numero_1=int(float(input("escribe un numero")))
+numero_2=int(float(imput("escribe un numero")))
+numero_3=int(float(imput("escrine un numero ")))
+numeros=[numeros_1, numero_2, numero3]
+print(numeros)
+sumatorio= sum(numeros)
+print(sumatorio)
+
+print("/n")
+guitarristas.pop(sg)
+print(futbolistas)
+
+marcas_coches.discard('aston martin')
+print(marcas_coches)
+
+print("/n")
+
+numero_1=int(float(input("escribe un numero")))
+numero_2=int(float(imput("escribe un numero")))
+numero_3=int(float(imput("escrine un numero ")))
+numeros=[numeros_1, numero_2, numero3]
+print(numeros)
+print(sumatorio)
+
+("/n")
+media=sumatorio/len(numeros)
+print(media)
+
+
+
+
+
+
+
+
+
+
